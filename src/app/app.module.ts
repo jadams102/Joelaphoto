59ed28ba5c9e71620b8b067f25440678acc953cd
Joel Adams <joeladamsdesign@gmail.com>
2018-03-30 08:47:50 -0700

    add bootstrap, start homepage navs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule
