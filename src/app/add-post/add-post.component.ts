25abe42d17accbe6a2eec9d551f880b8af2ed2bd
Joel Adams <joeladamsdesign@gmail.com>
2018-04-06 10:08:41 -0700

    refactor add post into separate component

--- src/app/add-post/add-post.component.ts
+++ src/app/add-post/add-post.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { Post } from '../models/post.model';
+import { PostService } from '../services/post.service';
 
 @Component({
   selector: 'app-add-post',
   templateUrl: './add-post.component.html',
-  styleUrls: ['./add-post.component.css']
+  styleUrls: ['./add-post.component.css'],
+  providers: [PostService]
 })
 export class AddPostComponent implements OnInit {
 
-  constructor() { }
+  constructor(private postService: PostService) { }
 
   ngOnInit() {
   }
 
+  submitPost(title: string, imgUrl: string, body: string) {
+    const currentDate = new Date();
+    const dateString = currentDate.getMonth() + '/' + currentDate.getDate() + '/' + currentDate.getFullYear();
+    const newPost = new Post(title, imgUrl, body);
+    newPost.postDate = dateString;
+    this.postService.addPost(newPost);
+  }
 }
--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,6 +2,7 @@ import { ModuleWithProviders } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { CreatorPageComponent } from './creator-page/creator-page.component';
+import { AddPostComponent } from './add-post/add-post.component';
 
 
 const appRoutes: Routes = [
@@ -10,8 +11,12 @@ const appRoutes: Routes = [
     component: HomepageComponent
   },
   {
-    path: 'creator',
+    path: 'admin',
     component: CreatorPageComponent
+  },
+  {
+    path: 'admin/add',
+    component: AddPostComponent
   }
 ];
 
